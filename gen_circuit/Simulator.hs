a659336b3e62984dfe6b09dc7637ef6504078024
Paul Berry <stereotype441@gmail.com>
2010-06-18 18:49:16 -0700

    Added a simulation of the example circuit so that we know what the proper prefix is.

--- gen_circuit/Simulator.hs
+++ gen_circuit/Simulator.hs
@@ -23,3 +23,33 @@ simulate (Just input, gates, Just output) input_sequence = output_sequence
                                                  | otherwise = 0 : (snd $ gate_outputs !! j)
           lookup i ExternalAddress = input_sequence
           output_sequence = lookup (length gates) output
+
+example_circuit :: Circuit
+example_circuit = (Just (InternalAddress 19 LeftSide),
+                   [Gate (Just (InternalAddress 12 RightSide)) (Just (InternalAddress 13 RightSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 14 RightSide)) (Just (InternalAddress 0 LeftSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 9 RightSide)) (Just (InternalAddress 10 RightSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 2 LeftSide)) (Just (InternalAddress 17 RightSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 15 RightSide)) (Just (InternalAddress 1 LeftSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 3 LeftSide)) (Just (InternalAddress 18 RightSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 5 LeftSide)) (Just (InternalAddress 11 RightSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 19 RightSide)) (Just (InternalAddress 16 RightSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 2 RightSide)) (Just (InternalAddress 7 RightSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 1 RightSide)) (Just (InternalAddress 3 RightSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 8 RightSide)) (Just (InternalAddress 4 LeftSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 8 LeftSide)) (Just (InternalAddress 7 LeftSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 6 RightSide)) (Just (InternalAddress 0 RightSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 6 LeftSide)) (Just (InternalAddress 4 RightSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 12 LeftSide)) (Just (InternalAddress 13 LeftSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 5 RightSide)) (Just (InternalAddress 11 LeftSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 10 LeftSide)) (Just (InternalAddress 15 LeftSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 14 LeftSide)) (Just (InternalAddress 16 LeftSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (InternalAddress 9 LeftSide)) (Just (InternalAddress 17 LeftSide)) (Just (ExternalAddress)) (Just (ExternalAddress)),
+                   Gate (Just (ExternalAddress)) (Just (InternalAddress 18 LeftSide)) (Just (ExternalAddress)) (Just (ExternalAddress))],
+                   Just (InternalAddress 19 LeftSide))
+
+example_input :: [Trit]
+example_input = [0,2,2,2,2,2,2,0,2,1,0,1,1,0,0,1,1]
+
+proper_prefix :: [Trit]
+proper_prefix = simulate example_circuit example_input
