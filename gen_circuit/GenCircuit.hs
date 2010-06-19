3f03b087d55ac59ac4a0fbc06a0184354f8d19d6
Paul Berry <stereotype441@gmail.com>
2010-06-18 17:57:23 -0700

    Fixed a merge problem.

--- gen_circuit/GenCircuit.hs
+++ gen_circuit/GenCircuit.hs
@@ -186,10 +186,6 @@ emitterWithCarry (x:xs) = (new_circuit, new_carry)
           new_carry   = x - carry
           (prev_circuit, carry) = emitterWithCarry xs
 
-tests = test [ "identity" ~: (showCircuit $ construct1to1Circuit identity) ~=? expected ]
-    where expected = "8L:\n7R8R0#1R1L,\n0R0L0#2L2R,\n1L1R0#3R3L,\n2R2L0#4L4R,\n3L3R0#5R5L,\n4R4L0#6R6L," ++ 
-                     "\n5R5L0#7R7L,\n6R6L0#8R0L,\nX7L0#X0R:\n8L"
-
 construct1to1Circuit :: SubCircuit -> Circuit
 construct1to1Circuit sub = constructCircuit $ input `chain` sub `chain` output
 
