0a308a6a70fb21e5d742bba42fd137a53a51ae4a
Jesse Wolfe <jes5199@gmail.com>
2010-06-20 21:55:41 -0700

    carFactory

--- gen_circuit/CreateCars.hs
+++ gen_circuit/CreateCars.hs
@@ -2,6 +2,7 @@ import Random
 import Control.Monad
 import System.IO.Unsafe
 import CarParts
+import FuelChecker
 
 randomList :: Int -> (StdGen -> (a, StdGen)) -> StdGen -> ([a], StdGen)
 randomList 0   func gen = ([], gen)
@@ -23,6 +24,26 @@ randomFuel ingredientCount gen = randomList 6 (randomFuelComponent ingredientCou
 
 randomPipe :: Int -> StdGen -> (Pipe, StdGen)
 randomPipe len = randomList len $ randomR (0,5)
+
+carFactory :: Int -> Int -> Fuel -> [(Pipe,[[Integer]])] -> [ReactionChamber] -> StdGen -> ((Car, Fuel), StdGen)
+carFactory 0 pipeLen fuel evaluated_pipes chambers gen = ((chambers, fuel), gen)
+carFactory counter pipeLen fuel evaluated_pipes chambers gen = carFactory (counter-1) pipeLen fuel evaluted_pipes' final_chambers gen'
+    where (pipe,gen')     = randomPipe pipeLen gen
+          matrix          = eval_pipe pipe fuel
+          new_chambers1   = concatMap (     makeValidReactionChambers (pipe,matrix) ) evaluated_pipes
+          new_chambers2   = concatMap (flip makeValidReactionChambers (pipe,matrix) ) evaluated_pipes
+          final_chambers  = new_chambrers1 ++ new_chambers2 ++ chambers
+          evaluated_pipes'= (pipe,matrix):evaluated_pipes
+
+matrixLessThanOrEqual :: [[Integer]] -> [[Integer]] -> Bool
+matrixLessThanOrEqual m1 m2 = all [ all [ a <= b | (a,b) <- zip row1 row2 ] | (row1,row2) <- zip m1 m2 ]
+
+makeValidReactionChambers :: (Pipe, [[Integer]]) -> (Pipe, [[Integer]]) -> [ReactionChamber]
+makeValidReactionChambers (p1, m1) (p2, m2) | not $ matrixLessThanOrEqual m1 m2 = []
+                                            | otherwise = [(p2, flag, p1)]
+                                                where flag = if m1 !! 0 !! 0 < m2 !! 0 !! 0
+                                                             then 0
+                                                             else 1
 
 main = do gen <- getStdGen 
           print $ randomPipe 10 gen
